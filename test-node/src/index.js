7230f864b0d715a5d4207d57f22ec65b23d0e024
megaserch <megaserch@gmail.com>
2021-04-26 11:39:12 -0300

    asdas

--- test-node/src/index.js
+++ test-node/src/index.js
@@ -1,20 +1,36 @@
 const puertoSerie = require('serialport');
+const fs = require('fs');
 
-const puerto = new puertoSerie("COM8", {
+verPuertos();
+
+/* puertoSerie.list().then(
+ports => ports.forEach( el => console.log(el)),
+err => console.log(err)
+);
+ */
+
+async function verPuertos(){
+    const listaPuertos = await puertoSerie.list();
+    console.log(listaPuertos);
+}
+
+/* const puerto = new puertoSerie("COM8", {
     baudRate: 115200,
-/*     dataBits: 8,
+    dataBits: 8,
     parity: none,
     stopBits : 1,
- */});
-
-puerto.open();
+});
+ */
+//puerto.open();
 
 /* puerto.on('readable', function () {
     console.log('Data:', puerto.read())
   }); */
 
-for (var o=0;o<100;o++){
+//puerto.write()
+
+/* for (var o=0;o<100;o++){
     console.log(puerto.read());
 }
-
-puerto.close();
+ */
+//puerto.close();
