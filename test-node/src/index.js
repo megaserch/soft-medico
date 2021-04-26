5c47578351e31599b916a77d2588c14eecb9cf90
megaserch <megaserch@gmail.com>
2021-04-26 12:08:44 -0300

    probando otra cosa

--- test-node/src/index.js
+++ test-node/src/index.js
@@ -47,14 +47,17 @@ port.open(function (err) {
   })
 
   // Read data that is available but keep the stream in "paused mode"
-port.on('readable', function () {
+/* port.on('readable', function () {
     console.log('Data readable:', port.read())
   })
   
   // Switches the port into "flowing mode"
   port.on('data', function (data) {
     console.log('Data data:', data)
-  })
+  }) */
   
   // Pipe the data into another stream (like a parser or standard out)
   //const lineStream = port.pipe(new Readline())
+
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+parser.on('data', console.log)
